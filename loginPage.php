5623a7411721d90f349b8e5813ab042bb94e0ab1
Gebrugidey <58293521+Gebrugidey@users.noreply.github.com>
2019-12-12 11:17:49 +0300

    few change is made

--- loginPage.php
+++ loginPage.php
@@ -4,5 +4,7 @@
     </head>
     <body>
         <h1>welcome to android development</h1>
+        <h2>this application is used for marketing management</h2>
+        <div>you can be a memeber of this appplication if you are willing to do</div>
     </body>
 </html>
