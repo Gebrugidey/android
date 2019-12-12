4017d10b21e521da82d17b5fda5f2434e52489a1
Gebrugidey <58293521+Gebrugidey@users.noreply.github.com>
2019-12-12 11:43:39 +0300

    adding branch

--- loginPage.php
+++ loginPage.php
@@ -6,6 +6,9 @@
         <h1>welcome to android development</h1>
         <h2>this application is used for marketing management</h2>
         <div>you can be a memeber of this appplication if you are willing to do</div>
-        
+        <p>mostly working with branch is ver exiting becouse it may not face any errors on the other hand working 
+            with master is unacceptable since it is the origninal it must keep change after testing.
+            having copy keeps you from unwnated tasks.
+        </p>
     </body>
 </html>
